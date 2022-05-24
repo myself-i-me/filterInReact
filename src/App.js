0061fc318ed6a1d83c54ec987afd9896f07cd102
Ganesh <ganeshdarode.intern@freshprints.com>
2022-05-24 16:45:08 +0530

    without random

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Images from "./Images";
+
 
 function App() {
+  const [data, setData] = useState(Images);
+  const [yes, setYes] = useState(0);
+
+  const selectCategory = (category) => {
+    const upDatedArray = Images.filter((element) => {
+      var matched=false;
+      for(var i of element.category){
+        if(i==category){
+          matched=true;
+          break;
+        }
+      }
+      return matched && element.withPhoto==yes;
+    });
+    setData(upDatedArray);
+  };
+
+  const change = (type) => {
+    setYes(type);
+    const upDatedArray = Images.filter((element) => {
+      return element.withPhoto === type;
+    });
+    setData(upDatedArray);
+    
+  };
+
+  const button = {
+    backgroundColor: "white",
+    padding: "10px",
+    borderRadius:"5px",
+    margin:"10px"
+  };
+
+  const buttonClass = {
+    display: "flex",
+    justfyContent: "center",
+  };
+
+  const lengthStyle={
+    border:"0px solid white",
+    backgroundColor:"white"
+  }
+
+
+  /*Randomizing the object*/
+
+  var listKeys = Object.keys(Images);
+  var randomIndex = Math.floor(Math.random() * listKeys.length);
+  var randomObject = Images[listKeys[randomIndex]];
+  console.log(listKeys);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="buttonclass" style={buttonClass}>
+        <button style={button} onClick={() => change(true)}>
+          With Photo
+        </button>
+        <button style={button} onClick={() => change(false)}>
+          WIthout Photo
+        </button>
+        <button style={button} onClick={() => selectCategory("Basic")}>
+          Basic
+        </button>
+        <button style={button} onClick={() => selectCategory("Creative")}>
+          Creative
+        </button>
+        <button style={button} onClick={() => selectCategory("Professional")}>
+          Professional
+        </button>
+        <button style={button} onClick={() => setData(Images)}>
+          All photos
+        </button>
+        <button style={lengthStyle}><h1>{data.length}</h1></button>
+      </div>
+      <div style={buttonClass}>
+        {data.map((element) => {
+          return (
+            <div
+              style={{
+                border: "2px grey solid",
+                margin: "10px",
+                width: "200px",
+              }}
+            >
+              <p>{element.name}</p>
+              <img
+                src={element.image}
+                alt=""
+                key={element.id}
+                width="200px"
+                height="300px"
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/Images.js
@@ -0,0 +1,75 @@
+const Images=[
+    {   
+        id:1,
+        image:"https://cdn-images.zety.com/templates/zety/cascade-3-duo-blue-navy-21@3x.png",
+        withPhoto:false,
+        category:["Basic","Professional","Creative"],
+        name:"NO-Photo & Basic-Professional-Creative"
+    },
+    {   
+        id:2,
+        image:"https://cdn-images.zety.com/templates/zety/concept-10-classic-blue-navy-312@3x.png",
+        withPhoto:false,
+        category:["Creative"],
+        name:"NO-PHOTO & Creative"
+    },
+    {
+        id:3,
+        image:"https://cdn-images.zety.com/templates/zety/crisp-15-classic-silver-dark-388@3x.png",
+        withPhoto:false,
+        category:["Creative"],
+        name:"NO-PHOTO & Creative"
+    },
+    {   
+        id:4,
+        image:"https://cdn-images.zety.com/templates/zety/cubic-20-trio-silver-dark-19@3x.png",
+        withPhoto:false,
+        category:["Professional"],
+        name:"NO-PHOTO & Professional"
+    },
+    {
+        id:5,
+        image:"https://cdn-images.zety.com/templates/zety/diamond-21-duo-silver-dark-999@3x.png",
+        withPhoto:false,
+        category:["Professional"],
+        name:"NO-PHOTO & Professional"
+    },
+    {
+        id:6,
+        image:"https://d1csarkz8obe9u.cloudfront.net/posterpreviews/resume-template-design-f24ac4fd0979cf1679e2d806e11eea93_screen.jpg?ts=1637037816",
+        withPhoto:true,
+        category:["Basic"],
+        name:"PHOTO & Basic"
+    },
+    {
+        id:7,
+        image:"https://i.pinimg.com/564x/17/a5/83/17a583853d9122e8588d3f3269a1937d.jpg",
+        withPhoto:true,
+        category:["Creative"],
+        name:"PHOTO & Creative"
+    },
+    {
+        id:8,
+        image:"https://i.pinimg.com/564x/e7/b1/64/e7b164663c4a644bc120a3903f701c55.jpg",
+        withPhoto:true,
+        category:["Creative"],
+        name:"PHOTO & Creative"
+    },
+    {
+        id:9,
+        image:"https://i.pinimg.com/564x/d3/5b/f9/d35bf97bf4a89b19604f4bc52762598c.jpg",
+        withPhoto:true,
+        category:["Professional"],
+        name:"PHOTO & Professional"
+    },
+    {
+        id:10,
+        image:"https://i.pinimg.com/564x/d5/5e/43/d55e4369736c2fd80a5af03a373c0d24.jpg",
+        withPhoto:true,
+        category:["Professional"],
+        name:"withPhoto & Professional"
+    }
+
+];
+
+export default Images;
